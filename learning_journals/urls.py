98452177425db299496e3d2f32f6b42ac3075dbc
Renato Bianco <renato.bianco@hotmail.com>
2021-11-11 19:48:38 +0000

    Added and updated new templates: index.html, base.html, topic.html, topics.html. Updated views.py  and urls.py. It's now possible to visit the topic page and its contents.

--- learning_journals/urls.py
+++ learning_journals/urls.py
@@ -4,15 +4,13 @@ from django.urls import path
 from . import views
 
 
-app_name = 'learning_journal'
+app_name = 'learning_journals'
 
 urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
+    # Page that shows all topics
+    path('topics/', views.topics, name='topics'),
+    # Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
-
-
-
-
-
-
--- learning_journals/views.py
+++ learning_journals/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+from .models import Topic
 
 
 def index(request):
     """Home Page for learning_journals"""
     return render(request, 'learning_journals/index.html')
+
+def topics(request):
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_journals/topics.html', context)
+
+def topic(request, topic_id):
+    """Show a single topic and all its entries"""
+    topic = Topic.objects.get(id=topic_id)
+    entries = topic.entry_set.order_by('-date_added')
+    context = {'topic':topic, 'entries':entries}
+    return render(request, 'learning_journals/topic.html', context)
+    
