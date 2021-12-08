5429a14384009474701d917ba69daa3736808ef7
Renato Bianco <renato.bianco@hotmail.com>
2021-12-08 13:31:58 +0000

    added edit_netry, created views and link in html

--- learning_journals/urls.py
+++ learning_journals/urls.py
@@ -17,4 +17,6 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
     # Page to add a new entry
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # Page to edit an entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
--- learning_journals/views.py
+++ learning_journals/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 def index(request):
@@ -59,3 +59,22 @@ def new_entry(request, topic_id):
     # Display a blank or invalid form
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_journals/new_entry.html', context)
+
+
+def edit_entry(request, entry_id):
+    """Edit an existing entry."""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    
+    if request.method != 'POST':
+        # Initial requestl pre-fill form with the current entry
+        form = EntryForm(instance=entry)
+    else:
+        # POST data submitted; process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_journals:topic', topic_id=topic.id)
+    
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request,'learning_journals/edit_entry.html', context)
