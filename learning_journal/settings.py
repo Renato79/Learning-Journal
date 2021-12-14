08c96c1ef3b431f45ff0dc6d192e043a87451d89
Renato Bianco <renato.bianco@hotmail.com>
2021-12-14 11:21:02 +0000

    Completed association between users and topics, completed user accounts

--- learning_journal/settings.py
+++ learning_journal/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps.
     'learning_journals',
+    'users',
     
     # Default django apps.
     'django.contrib.admin',
@@ -127,3 +128,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# My settings
+LOGIN_URL = 'users:login'
--- learning_journals/views.py
+++ learning_journals/views.py
@@ -1,4 +1,8 @@
+from django.contrib.auth import login
+from django.http.response import Http404
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -8,21 +12,25 @@ def index(request):
     return render(request, 'learning_journals/index.html')
 
 
+@login_required
 def topics(request):
     """Show all topics."""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_journals/topics.html', context)
 
-
+@login_required
 def topic(request, topic_id):
     """Show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
+    # Make sure the topic belongs to the current user.
+    if topic.owner != request.user:
+        raise Http404
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_journals/topic.html', context)
 
-
+@login_required
 def new_topic(request):
     """Add a new topic"""
     if request.method != 'POST':
@@ -32,14 +40,16 @@ def new_topic(request):
         # POST data submitted; process data
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('learning_journals:topics')
     
     # Display a blank or invalid form
     context = {'form': form}
     return render(request, 'learning_journals/new_topic.html', context)
 
-
+@login_required
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
@@ -60,11 +70,14 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_journals/new_entry.html', context)
 
-
+@login_required
 def edit_entry(request, entry_id):
     """Edit an existing entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    
+    if topic.owner != request.user:
+        raise Http404
     
     if request.method != 'POST':
         # Initial requestl pre-fill form with the current entry
