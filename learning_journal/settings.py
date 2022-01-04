81dcee45485fdb5d69e6877ab4c613a4fd7347b9
Renato Bianco <renato.bianco@hotmail.com>
2022-01-04 15:04:24 +0000

    Completed the backend. Installed Bootstrap5. Styled pages base.html, index.html, login.html.

--- learning_journal/settings.py
+++ learning_journal/settings.py
@@ -35,6 +35,9 @@ INSTALLED_APPS = [
     'learning_journals',
     'users',
     
+    # Third party apps
+    'bootstrap5',
+    
     # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
