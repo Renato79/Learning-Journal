0225b0ee473d444757b53790cd58b10ec85bfc2a
Renato Bianco <renato.bianco@hotmail.com>
2022-03-04 22:11:14 +0000

    Replaced Secret key, and changed Debug mode. Deployment completed

--- learning_journal/settings.py
+++ learning_journal/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7g(ibc2=5=w-@$9gwb^o7=%*%cxg!dx^#f-ssi7t9)m9qee=a7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['learning-journal-online.herokuapp.com']
 
